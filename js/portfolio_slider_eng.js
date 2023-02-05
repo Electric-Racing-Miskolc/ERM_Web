5a60c7902d728241073df4d8ef63baffef975532
Dávid Sikora <90835045+Redd-15@users.noreply.github.com>
2023-02-05 14:35:46 +0100

    Added Webpage Files

---
+++ js/portfolio_slider_eng.js
@@ -0,0 +1,80 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'EuroSolid Zrt. Sponsoration',
+			'description'   : 'We would like to thank EuroSolid Zrt. for their support and help in which we\'ve got access to the 3DEXPERIENCE platform and many other utilities, as well as training, so we can monitor our work in a cloud-based system. 2022.12.08.',
+			'thumbnail' : ['img/portfolio/small/No5.jpg'],
+			'large' : ['img/portfolio/large/No5.jpg'],
+			'button_list'   :
+			[
+				{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=125757393670825&set=a.116351514611413' },
+				{ 'title':'Instagram', 'url':'https://www.instagram.com/p/Cl6CmfEqj99/'},
+				{ 'title':'EuroSolid Zrt.', 'url':'https://eurosolid.hu/'}
+			],
+			'tags'  : ['Sponsored','All']
+			},
+
+			{
+			'title' : 'UniTek Sponsoration',
+			'description'   : 'We would like to thank Unitek Drives for supporting us in building our electric car and Miskolci Egyetem (University of Miskolc) for their help with the purchase. 2022.11.21.',
+			'thumbnail' : ['img/portfolio/small/No4.jpg'],
+			'large' : ['img/portfolio/large/No4.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=120098677570030&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/ClPC3yHMOL4/'},
+			{ 'title':'Unitek', 'url':'https://www.unitek-industrie-elektronik.de/'}
+			],
+			'tags'  : ['Sponsored','All']
+			},
+
+			{
+			'title' : 'University Sponsored Tools',
+			'description'   : 'Thanks to Miskolci Egyetem (University of Miskolc) for their support in setting up the team and equipping our workshop. 2022.11.16.',
+			'thumbnail' : ['img/portfolio/small/No3.jpg'],
+			'large' : ['img/portfolio/large/No3.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=118240334422531&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/ClCNqc0sEoW/'}
+			],
+			'tags'  : ['Sponsored', 'All']
+			},
+
+			{
+			'title' : 'Team Startup Interview',
+			'description'   : 'Electric Racing Miskolc is an enthusiastic, young and creative team. What are they doing? Details in our video. And you can still join. 2022.10.13.',
+			'thumbnail' : ['img/portfolio/small/No2_1.jpg', 'img/portfolio/small/No2_2.jpg'],
+			'large' : ['img/portfolio/large/No2_1.jpg', 'img/portfolio/large/No2_2.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook Video Post (HUN)', 'url' : 'https://www.facebook.com/unimiskolc/videos/452365386989036' },
+			{ 'title':'University Of Miskolc', 'url':'http://www.uni-miskolc.hu'}
+			],
+			'tags'  : ['Team','All']
+			},
+
+			{
+			'title' : 'The Beginning...',
+			'description'   : 'We are happy to announce that the first fully electric Formula Student racecar engineering team of the University of Miskolc has been formed! Our team by the name Electric Racing Miskolc, representing the University of Miskolc, is competing in the Formula Student international racing series. 2022.11.11.',
+			'thumbnail' : ['img/portfolio/small/No1.jpg'],
+			'large' : ['img/portfolio/large/No1.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=116351501278081&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/Ck1AipaKObb/'},
+			{ 'title':'University Of Miskolc', 'url':'http://www.uni-miskolc.hu'}
+			],
+			'tags'  : ['Team','All']
+			},
+	 
+		]
+	});
+});
---
+++ js/portfolio_slider_hun.js
@@ -0,0 +1,80 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'EuroSolid Zrt. Szponzoráció',
+			'description'   : 'Szeretnénk megköszönni az EuroSolid Zrt.-nek a támogatást, akik segítségével a 3DEXPERIENCE platformhoz és számos más segédprogramhoz hozzáférést, illetve oktatást kapunk, így egy felhő alapú rendszerben tudjuk nyomon követni munkánkat. 2022.12.08.',
+			'thumbnail' : ['img/portfolio/small/No5.jpg'],
+			'large' : ['img/portfolio/large/No5.jpg'],
+			'button_list'   :
+			[
+				{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=125757393670825&set=a.116351514611413' },
+				{ 'title':'Instagram', 'url':'https://www.instagram.com/p/Cl6CmfEqj99/'},
+				{ 'title':'EuroSolid Zrt.', 'url':'https://eurosolid.hu/'}
+			],
+			'tags'  : ['Szponzoráció','Mind']
+			},
+
+			{
+			'title' : 'UniTek Szponzoráció',
+			'description'   : 'Ezúton szeretnénk megköszönni az Unitek Drives cégének, hogy támogatt elektromos autónk építésében, illetve a Miskolci Egyetem beszerzésben nyújtott segítségét. 2022.11.21.',
+			'thumbnail' : ['img/portfolio/small/No4.jpg'],
+			'large' : ['img/portfolio/large/No4.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=120098677570030&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/ClPC3yHMOL4/'},
+			{ 'title':'Unitek', 'url':'https://www.unitek-industrie-elektronik.de/'}
+			],
+			'tags'  : ['Szponzoráció','Mind']
+			},
+
+			{
+			'title' : 'Egyetemi szerszám szponzoráció',
+			'description'   : 'Köszönjük a Miskolci Egyetem támogatását a csapat létrehozásában és a műhelyünk felszerelésében. 2022.11.16',
+			'thumbnail' : ['img/portfolio/small/No3.jpg'],
+			'large' : ['img/portfolio/large/No3.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=118240334422531&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/ClCNqc0sEoW/'}
+			],
+			'tags'  : ['Szponzoráció', 'Mind']
+			},
+
+			{
+			'title' : 'Csapat alapítási interjú',
+			'description'   : 'Lelkes, fiatal és kreatív csapat az Electric Racing Miskolc. Mit csinálnak? Videónkból kiderül. És még lehet csatlakozni. 2022.10.13',
+			'thumbnail' : ['img/portfolio/small/No2_1.jpg', 'img/portfolio/small/No2_2.jpg'],
+			'large' : ['img/portfolio/large/No2_1.jpg', 'img/portfolio/large/No2_2.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook Video Post (HUN)', 'url' : 'https://www.facebook.com/unimiskolc/videos/452365386989036' },
+			{ 'title':'University Of Miskolc', 'url':'http://www.uni-miskolc.hu'}
+			],
+			'tags'  : ['Csapat','Mind']
+			},
+
+			{
+			'title' : 'The Beginning...',
+			'description'   : 'Örömmel jelentjük be, hogy megalakult a Miskolci Egyetem első, 100 %-ban elektromos meghajtású versenyautót fejlesztő Formula Student csapata! Csapatunk Electric Racing Miskolc néven, a Miskolci Egyetemet képviselve méretteti meg magát a Formula Student nemzetközi hallgatói versenysorozatban. 2022.11.11.',
+			'thumbnail' : ['img/portfolio/small/No1.jpg'],
+			'large' : ['img/portfolio/large/No1.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=116351501278081&set=a.116351514611413' },
+			{ 'title':'Instagram', 'url':'https://www.instagram.com/p/Ck1AipaKObb/'},
+			{ 'title':'University Of Miskolc', 'url':'http://www.uni-miskolc.hu'}
+			],
+			'tags'  : ['Csapat','Mind']
+			},
+	 
+		]
+	});
+});
