444617ff2b4cd92a86379590c638871a7f8aba90
SIgabor <79259040+SIgabor@users.noreply.github.com>
2023-02-17 12:08:37 +0100

    Compression Test

--- js/portfolio_slider_hun.js
+++ js/portfolio_slider_hun.js
@@ -10,8 +10,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'Megérkezett az Emrax 228 MV Motorunk!',
 			'description'   : 'Csapatunk megvette első elektromos autójának motorját. Szeretnénk megköszönni a Miskolci Egyetem Gépészmérnöki és Informatikai Karának a finanszírozást, valamint a PUT Motorsport-nak az együttműködést! 2023.02.13.',
-			'thumbnail' : ['img/portfolio/small/No6.jpg'],
-			'large' : ['img/portfolio/large/No6.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No6.gif'],
+			'large' : ['img_smoll/portfolio/large/No6.gif'],
 			'button_list'   :
 			[
 				{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo/?fbid=146044701642094&set=a.116351514611413' },
@@ -23,8 +23,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'EuroSolid Zrt. Szponzoráció',
 			'description'   : 'Szeretnénk megköszönni az EuroSolid Zrt.-nek a támogatást, akik segítségével a 3DEXPERIENCE platformhoz és számos más segédprogramhoz hozzáférést, illetve oktatást kapunk, így egy felhő alapú rendszerben tudjuk nyomon követni munkánkat. 2022.12.08.',
-			'thumbnail' : ['img/portfolio/small/No5.jpg'],
-			'large' : ['img/portfolio/large/No5.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No5.gif'],
+			'large' : ['img_smoll/portfolio/large/No5.gif'],
 			'button_list'   :
 			[
 				{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=125757393670825&set=a.116351514611413' },
@@ -37,8 +37,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'UniTek Szponzoráció',
 			'description'   : 'Ezúton szeretnénk megköszönni az Unitek Drives cégének, hogy támogatt elektromos autónk építésében, illetve a Miskolci Egyetem beszerzésben nyújtott segítségét. 2022.11.21.',
-			'thumbnail' : ['img/portfolio/small/No4.jpg'],
-			'large' : ['img/portfolio/large/No4.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No4.gif'],
+			'large' : ['img_smoll/portfolio/large/No4.gif'],
 			'button_list'   :
 			[
 			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=120098677570030&set=a.116351514611413' },
@@ -51,8 +51,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'Egyetemi szerszám szponzoráció',
 			'description'   : 'Köszönjük a Miskolci Egyetem támogatását a csapat létrehozásában és a műhelyünk felszerelésében. 2022.11.16',
-			'thumbnail' : ['img/portfolio/small/No3.jpg'],
-			'large' : ['img/portfolio/large/No3.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No3.gif'],
+			'large' : ['img_smoll/portfolio/large/No3.gif'],
 			'button_list'   :
 			[
 			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=118240334422531&set=a.116351514611413' },
@@ -64,8 +64,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'Csapat alapítási interjú',
 			'description'   : 'Lelkes, fiatal és kreatív csapat az Electric Racing Miskolc. Mit csinálnak? Videónkból kiderül. És még lehet csatlakozni. 2022.10.13',
-			'thumbnail' : ['img/portfolio/small/No2_1.jpg', 'img/portfolio/small/No2_2.jpg'],
-			'large' : ['img/portfolio/large/No2_1.jpg', 'img/portfolio/large/No2_2.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No2_1.gif', 'img_smoll/portfolio/small/No2_2.gif'],
+			'large' : ['img_smoll/portfolio/large/No2_1.gif', 'img_smoll/portfolio/large/No2_2.gif'],
 			'button_list'   :
 			[
 			{ 'title':'Facebook Video Post (HUN)', 'url' : 'https://www.facebook.com/unimiskolc/videos/452365386989036' },
@@ -77,8 +77,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			'title' : 'The Beginning...',
 			'description'   : 'Örömmel jelentjük be, hogy megalakult a Miskolci Egyetem első, 100 %-ban elektromos meghajtású versenyautót fejlesztő Formula Student csapata! Csapatunk Electric Racing Miskolc néven, a Miskolci Egyetemet képviselve méretteti meg magát a Formula Student nemzetközi hallgatói versenysorozatban. 2022.11.11.',
-			'thumbnail' : ['img/portfolio/small/No1.jpg'],
-			'large' : ['img/portfolio/large/No1.jpg'],
+			'thumbnail' : ['img_smoll/portfolio/small/No1.gif'],
+			'large' : ['img_smoll/portfolio/large/No1.gif'],
 			'button_list'   :
 			[
 			{ 'title':'Facebook', 'url' : 'https://www.facebook.com/photo?fbid=116351501278081&set=a.116351514611413' },
