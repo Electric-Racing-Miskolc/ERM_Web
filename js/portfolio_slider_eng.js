50ed6eaf7deba2d3411b0cb3197616a8836b6820
TDaniel <tyuska85@gmail.com>
2023-12-11 20:36:36 +0100

    Loctite sponsoration
    Loctite sponsoration

--- js/portfolio_slider_eng.js
+++ js/portfolio_slider_eng.js
@@ -8,6 +8,17 @@ $("#elastic_grid_demo").elastic_grid({
 	'items' :
 		[
 			{
+				'title' : 'LOCTITE Sponsoration',
+				'description'   : 'As our team is building an electric race car, we have come to rely on LOCTITE SF 7039 Contact Cleaner to provide a reliable and effective solution for cleaning electrical contacts, displacing moisture and other contaminants to help keep our cars running and performing at their best. We would like to thank the Loctite  team for helping us with this product.🏎⚡ 2023.12.05.',
+				'thumbnail' : ['../img/portfolio/small/No25.webp'],
+				'large' : ['../img/portfolio/large/No25.webp'],
+				'button_list'   :
+					[
+						{ 'title':'LOCTITE', 'url' : 'https://www.henkel-adhesives.com/hu/hu/rólunk/márkáink/loctite.html' },
+					],
+				'tags'  : ['Sponsored','All']
+			},
+			{
 				'title' : 'TE Connectivity Sponsoration',
 				'description'   : 'We would like to thank TE Connectivity for providing the high voltage connectors and other electronic components necessary for the construction of our race car.🦾 2023.11.13.',
 				'thumbnail' : ['../img/portfolio/small/No24.webp'],
