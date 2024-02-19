839286d6fccaba505a26fb04d35f4c51065172e4
SIgabor <79259040+SIgabor@users.noreply.github.com>
2024-02-19 11:44:06 +0100

    insta feed limited

--- js/instafeed.js
+++ js/instafeed.js
@@ -2,6 +2,7 @@ var userFeed = new Instafeed({
     get: 'slyizsistvangabor',
     target: "instafeed-container",
     resolution: 'low_resolution',
-    accessToken: 'IGQWROQnNnRUxHbUJUWDdZAQmJreHhCVC1tS1gyVnZAsNzhXcjdqVDl3d1RjUEZA3RVdZAZAUZAnenNnVXVuN0V6SnI5R1BiWm5NdUNwbVlVeDJNSHZA3Nk5abE15ZAm9STzl6M0Y0TGdwTXMzWi03UV9oQnZAmSHl6a2lLY2sZD'
+    accessToken: 'IGQWRNMk9jWkRSZAmNId0pNZAzhWVmlFTk9mV2NmeVoyYzlITjIyYVBXUWNYaE9LMmd3ZAWs5bnQzU0JTU1FxbUxyYmtFY0lEdEk1cDgtZAk1IeVVpQXVURFVxNXFZASW1LemRqczFMYndtMjZAIMmJ6OFFqX2dkbUg2ZAk0ZD'
 });
+console.log('ok');
 userFeed.run();
