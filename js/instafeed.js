32c72510933636f479dbd2ed1f5c9dc5edf6f4f4
Sikora Dávid <sikoradavid1234@gmail.com>
2024-02-22 11:26:57 +0100

    insta name orreted + deleted portfolio samples

--- js/instafeed.js
+++ js/instafeed.js
@@ -1,5 +1,5 @@
 var userFeed = new Instafeed({
-    get: 'slyizsistvangabor',
+    get: 'ermiskolc',
     target: "instafeed-container",
     resolution: 'low_resolution',
     accessToken: 'IGQWRNMk9jWkRSZAmNId0pNZAzhWVmlFTk9mV2NmeVoyYzlITjIyYVBXUWNYaE9LMmd3ZAWs5bnQzU0JTU1FxbUxyYmtFY0lEdEk1cDgtZAk1IeVVpQXVURFVxNXFZASW1LemRqczFMYndtMjZAIMmJ6OFFqX2dkbUg2ZAk0ZD'
